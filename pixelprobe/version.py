9b943b4d80dc15f823522d96829a44d7f62e4727
ttlequals0 <dkrachtus@ttlequals0.com>
2026-03-20 16:30:57 -0400

    Bump version to 2.6.2 for celery worker log handler fix

--- pixelprobe/version.py
+++ pixelprobe/version.py
@@ -4,7 +4,7 @@ import os
 # Default version - this is the single source of truth
 
 
-_DEFAULT_VERSION = '2.6.1'
+_DEFAULT_VERSION = '2.6.2'
 
 
 # Allow override via environment variable for CI/CD, but default to the hardcoded version
